a9ea872deb6296e66b32a899c0c64b1d1ea5f025
이지영 <85738654+jeeyoung3189@users.noreply.github.com>
2023-01-04 21:46:27 +0900

    그누보드 설치

---
+++ head.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+run_event('pre_head');
+
+include_once(G5_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/poll.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+?>
---
+++ tail.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+?>
+
+<?php
+include_once(G5_PATH."/tail.sub.php");
+?>
